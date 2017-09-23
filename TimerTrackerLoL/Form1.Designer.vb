9571a80803290d606cacd7ba4d1e459f80ad5aec
Juri Adams <juri@4da.ms>
2017-09-23 05:35:50 +0200

    First push

---
+++ TimerTrackerLoL/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InGameTracker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InGameTracker))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.SummSpell1 = New System.Windows.Forms.Label()
+        Me.SummSpell2 = New System.Windows.Forms.Label()
+        Me.SummSpell3 = New System.Windows.Forms.Label()
+        Me.SummSpell4 = New System.Windows.Forms.Label()
+        Me.SummSpell5 = New System.Windows.Forms.Label()
+        Me.SummSpell6 = New System.Windows.Forms.Label()
+        Me.Summ1SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Summ2SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Summ3SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Summ4SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Summ5SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Summ6SpellCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.Summ1SpellReadyCountdown = New System.Windows.Forms.Timer(Me.components)
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Franklin Gothic Medium", 20.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(69, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(304, 34)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Summoner Spell Tracker"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell3)
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell4)
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell5)
+        Me.FlowLayoutPanel1.Controls.Add(Me.SummSpell6)
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(35, 12)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(377, 63)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'SummSpell1
+        '
+        Me.SummSpell1.AutoSize = True
+        Me.SummSpell1.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell1.Location = New System.Drawing.Point(3, 0)
+        Me.SummSpell1.Name = "SummSpell1"
+        Me.SummSpell1.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell1.TabIndex = 0
+        '
+        'SummSpell2
+        '
+        Me.SummSpell2.AutoSize = True
+        Me.SummSpell2.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell2.Location = New System.Drawing.Point(3, 15)
+        Me.SummSpell2.Name = "SummSpell2"
+        Me.SummSpell2.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell2.TabIndex = 1
+        '
+        'SummSpell3
+        '
+        Me.SummSpell3.AutoSize = True
+        Me.SummSpell3.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell3.Location = New System.Drawing.Point(3, 30)
+        Me.SummSpell3.Name = "SummSpell3"
+        Me.SummSpell3.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell3.TabIndex = 2
+        '
+        'SummSpell4
+        '
+        Me.SummSpell4.AutoSize = True
+        Me.SummSpell4.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell4.Location = New System.Drawing.Point(3, 45)
+        Me.SummSpell4.Name = "SummSpell4"
+        Me.SummSpell4.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell4.TabIndex = 3
+        '
+        'SummSpell5
+        '
+        Me.SummSpell5.AutoSize = True
+        Me.SummSpell5.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell5.Location = New System.Drawing.Point(9, 0)
+        Me.SummSpell5.Name = "SummSpell5"
+        Me.SummSpell5.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell5.TabIndex = 4
+        '
+        'SummSpell6
+        '
+        Me.SummSpell6.AutoSize = True
+        Me.SummSpell6.Font = New System.Drawing.Font("Franklin Gothic Medium", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummSpell6.Location = New System.Drawing.Point(9, 15)
+        Me.SummSpell6.Name = "SummSpell6"
+        Me.SummSpell6.Size = New System.Drawing.Size(0, 15)
+        Me.SummSpell6.TabIndex = 5
+        '
+        'Summ1SpellCounter
+        '
+        Me.Summ1SpellCounter.Interval = 1000
+        '
+        'Summ2SpellCounter
+        '
+        Me.Summ2SpellCounter.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 3
+        '
+        'Summ3SpellCounter
+        '
+        Me.Summ3SpellCounter.Interval = 1000
+        '
+        'Summ4SpellCounter
+        '
+        Me.Summ4SpellCounter.Interval = 1000
+        '
+        'Summ5SpellCounter
+        '
+        Me.Summ5SpellCounter.Interval = 1000
+        '
+        'Summ6SpellCounter
+        '
+        Me.Summ6SpellCounter.Interval = 1000
+        '
+        'Summ1SpellReadyCountdown
+        '
+        Me.Summ1SpellReadyCountdown.Interval = 1000
+        '
+        'InGameTracker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(448, 79)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.Label3)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "InGameTracker"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "League Timer Tracker"
+        Me.TransparencyKey = System.Drawing.Color.Black
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents SummSpell1 As Label
+    Friend WithEvents Summ1SpellCounter As Timer
+    Friend WithEvents SummSpell2 As Label
+    Friend WithEvents SummSpell3 As Label
+    Friend WithEvents Summ2SpellCounter As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Summ3SpellCounter As Timer
+    Friend WithEvents Summ4SpellCounter As Timer
+    Friend WithEvents Summ5SpellCounter As Timer
+    Friend WithEvents SummSpell4 As Label
+    Friend WithEvents Summ6SpellCounter As Timer
+    Friend WithEvents SummSpell5 As Label
+    Friend WithEvents SummSpell6 As Label
+    Friend WithEvents Summ1SpellReadyCountdown As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ TimerTrackerLoL/Main.vb
@@ -0,0 +1,173 @@
+﻿Public Class Main
+    Dim start As Boolean = True
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If findtxt.ShowDialog = DialogResult.OK Then
+            My.Settings.textpath = findtxt.FileName
+            InGameTracker.path = findtxt.FileName
+            txtBoxloc.Text = findtxt.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If start = True Then
+            ' X position for centered above scoreboard: 730
+            ' Y position for centered above scoreboard: 155
+            ' X position for centered above status hud: 750
+            ' Y position for centered above status hud: 900
+            MsgBox("Make sure to start League Of Legends in borderless mode, else wise this will not work!", MsgBoxStyle.Information)
+            Label7.Text = "Change ingame position:"
+            InGameTracker.Show()
+            start = False
+            Button2.Text = "Stop"
+            winposX.Enabled = False
+            winposY.Enabled = False
+        ElseIf start = False Then
+            InGameTracker.Close()
+            start = True
+            Label7.Text = "Change ingame position:"
+            Button2.Text = "Start"
+            winposX.Enabled = True
+            winposY.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        winposX.Text = My.Settings.winposx
+        winposY.Text = My.Settings.winposy
+
+        If My.Settings.textpath = "" Then
+            Button2.Enabled = False
+        Else
+            Button2.Enabled = True
+            txtBoxloc.Text = My.Settings.textpath
+            InGameTracker.path = My.Settings.textpath
+        End If
+        InGameTracker.Close()
+    End Sub
+
+    Private Sub winposX_TextChanged(sender As Object, e As EventArgs) Handles winposX.TextChanged
+        If winposX.Text = "" Then
+
+        Else
+            Dim stringToIntegerX As Integer = Integer.Parse(winposX.Text)
+            My.Settings.winposx = stringToIntegerX
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub winposY_TextChanged(sender As Object, e As EventArgs) Handles winposY.TextChanged
+        If winposY.Text = "" Then
+        Else
+            Dim stringToIntegerY As Integer = Integer.Parse(winposY.Text)
+            My.Settings.winposy = stringToIntegerY
+            My.Settings.Save()
+        End If
+
+    End Sub
+
+    Private Sub findtxt_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles findtxt.FileOk
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If My.Computer.FileSystem.FileExists(My.Settings.textpath) Then
+            If My.Settings.textpath.EndsWith(".txt") Then
+                Button2.Enabled = True
+                exist.Text = ""
+            Else
+                Button2.Enabled = False
+                exist.Text = "Something is wrong with the file. Is it a .txt?"
+            End If
+
+        Else
+            Button2.Enabled = False
+            exist.Text = "MyNotes.txt not found!"
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        MessageBox.Show("The default file-path is:" & vbCrLf & """C:\Riot Games\League Of Legends\RADS\solutions\lol_game_client_sln\releases\X.X.X.X\MyNotes.txt""" & vbCrLf & vbCrLf & "If the file doesn't exist yet, head ingame and type ""/note hello"", to create a new file.")
+    End Sub
+
+    Private Sub txtBoxloc_TextChanged(sender As Object, e As EventArgs) Handles txtBoxloc.TextChanged
+        My.Settings.textpath = txtBoxloc.Text
+    End Sub
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Label8_MouseMove(sender As Object, e As MouseEventArgs) Handles Label8.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Label8_MouseUp(sender As Object, e As MouseEventArgs) Handles Label8.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Label8_MouseDown(sender As Object, e As MouseEventArgs) Handles Label8.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        MessageBox.Show("After entering the correct location of your MyNotes.txt file, click Start." & vbCrLf & vbCrLf & "Now, launch a game. You should see Timer Tracker show at the top left of your screen." & vbCrLf & vbCrLf & "Once in game, type in chat ""/n ez f""." & vbCrLf & vbCrLf & "A timer for Ezreals flash should start." & vbCrLf & vbCrLf & "Supported Summoner Spells: Flash (f); Barrier(b); Ignite(i); Heal(h); Exhaust(e); Cleanse(c)")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+End Class
